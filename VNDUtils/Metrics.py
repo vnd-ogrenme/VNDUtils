d6c266693321c18c5a10c9d60abeeb0969eebf6b
Aykut G. Gelen <aggelen@outlook.com>
2024-08-25 18:03:33 +0300

    commit one

---
+++ VNDUtils/Metrics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 17:57:21 2024
+
+@author: gelenag
+"""
+
+def firing_rate():
+    pass
---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 17:57:29 2024
+
+@author: gelenag
+"""
+
+from VNDUtils.Metrics import firing_rate
+
+firing_rate()
